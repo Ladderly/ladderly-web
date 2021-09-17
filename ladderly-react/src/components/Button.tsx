f2200b6fc20a23ea1eeba0100fc58f93d97ee129
Lakshya Kumar <52732477+Lakshya-coder12@users.noreply.github.com>
2021-09-17 23:29:00 +0530

    Revamped Button component (#11)
    * changed color configuration
    
    * changed prop name and themeclass

--- ladderly-react/src/components/Button.tsx
+++ ladderly-react/src/components/Button.tsx
@@ -1,24 +1,33 @@
-import { ButtonHTMLAttributes, FC } from 'react'
-import  {memo} from 'react'
+import { ButtonHTMLAttributes, FC } from "react";
+import { memo } from "react";
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement>{
-  theme?: 'primary'|'secondary';
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  theme?: "outline" | "fill";
   children: string;
 }
 
-const Button: FC<Props> = ({theme, children, className, ...rest}) => {
-   const themeClasses = theme === "primary" ? "bg-secondary text-white" : "hover:text-skytext  border-black md:border-2";
- return(
+const Button: FC<Props> = ({ theme, children, className, ...rest }) => {
+  const themeClasses =
+    theme === "fill"
+      ? "bg-secondary-500 text-white hover:bg-secondary-300"
+      : "hover:text-secondary-500  border-black md:border-2";
+  return (
     <div>
-      <button 
-         {...rest} 
-         className={"rounded-full py-2 px-3 uppercase text-xs font-bold cursor-pointer tracking-wider " + themeClasses + " " + className}>
-         {children}
+      <button
+        {...rest}
+        className={
+          "rounded-full py-2 px-3 uppercase text-xs font-bold cursor-pointer tracking-wider " +
+          themeClasses +
+          " " +
+          className
+        }
+      >
+        {children}
       </button>
     </div>
- );
+  );
 };
 Button.defaultProps = {
-   theme: 'primary',
-}
+  theme: "fill",
+};
 export default memo(Button);
--- ladderly-react/tailwind.config.js
+++ ladderly-react/tailwind.config.js
@@ -1,19 +1,25 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      backgroundColor:theme => ({
-        'secondary':'#2CDCE1',
-      }),
-      textColor: theme => theme('colors'),
-     textColor: {
-       'skytext':'#2CDCE1'
-     },
+    colors: {
+      secondary: {
+        100: "#e9fbfc",
+        200: "#bcf4f5",
+        300: "#8fecef",
+        400: "#63e5e9",
+        500: "#36dee2",
+      },
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
     },
+    extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
