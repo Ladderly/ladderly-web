cabeb62c0e249e2f3f77f32375772bf99f2eb73a
Lakshya Kumar <lkkumar544@gmail.com>
2021-11-14 17:31:09 +0530

    Initial setup

---
+++ ladderly-react/tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      secondary: {
+        100: "#39D2BE",
+        200: "#53C7B3",
+        300: "#29BAA1",
+        400: "#06705D",
+      },
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
+    },
+    extend: {
+      fontSize: {
+        "6xl": "4.375rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
