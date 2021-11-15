a6a4b249c30d6a5362296cf2c3747b421585c4ef
Lakshya Kumar <52732477+Lakshya-coder12@users.noreply.github.com>
2021-11-15 16:42:43 +0530

    Add dynamic slides to carousel (#34)
    * Add SVG's
    
    * Add dynamic slides to carousel

--- ladderly-react/src/components/Carousel/CarouselItems/HeroSection.tsx
+++ ladderly-react/src/components/Carousel/CarouselItems/HeroSection.tsx
@@ -1,24 +1,54 @@
 import React, { FC, memo } from "react";
-import hero_section from "../../../img/hero-section.png";
+import { ReactComponent as Custom } from "../../../assets/svg/customization.svg";
+import { ReactComponent as Ladder } from "../../../assets/svg/ladder.svg";
+import { ReactComponent as Community } from "../../../assets/svg/community.svg";
 
-interface Props {}
+interface Props {
+  icon: "ladder" | "custom" | "community";
+}
 
-const HeroSection: FC<Props> = (props) => {
+const HeroSection: FC<Props> = ({ icon }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full h-auto md:flex-row">
-      <div className="flex-1 mb-10 ml-0 text-3xl font-black sm:text-5xl md:ml-40 md:text-4xl lg:text-5xl xl:text-6xl">
-        <h1 className="text-secondary-300">Our Ladders</h1>
-        <h1 className="text-gray-600 ">have got</h1>
-        <h1 className="text-gray-600 ">
-          you <span className="text-secondary-300">covered.</span>
-        </h1>
+      <div className="flex-1 p-12 mx-auto text-3xl font-black md:mb-10 md:p-0 sm:text-5xl md:ml-40 md:text-4xl lg:text-5xl xl:text-6xl">
+        {icon === "custom" && (
+          <>
+            <h1 className="text-secondary-300">Customize</h1>
+            <h1 className="text-gray-600 ">ladders</h1>
+            <h1 className="text-gray-600 ">
+              according to your{" "}
+              <span className="text-secondary-300">need.</span>
+            </h1>
+          </>
+        )}
+        {icon === "ladder" && (
+          <>
+            <h1 className="text-secondary-300">Our ladders</h1>
+            <h1 className="text-gray-600 ">have got</h1>
+            <h1 className="text-gray-600 ">
+              you <span className="text-secondary-300">covered.</span>
+            </h1>
+          </>
+        )}
+        {icon === "community" && (
+          <>
+            <h1 className="text-gray-600">Join our</h1>
+            <h1 className="text-secondary-300">Community</h1>
+            <h1 className="text-gray-600 ">
+              to help others and get your doubts{" "}
+              <span className="text-secondary-300">cleared.</span>
+            </h1>
+          </>
+        )}
       </div>
-      <div className="flex-1">
-        <img
-          className="object-cover w-full sm:h-1/2"
-          src={hero_section}
-          alt="ladder"
-        />
+      <div className="flex-1 w-full p-12">
+        {icon === "custom" ? (
+          <Custom className="w-4/5 mx-auto h-4/5 sm:h-1/2" />
+        ) : icon === "ladder" ? (
+          <Ladder className="w-4/5 mx-auto h-4/5 sm:h-1/2" />
+        ) : (
+          <Community className="w-4/5 mx-auto h-4/5 sm:h-1/2" />
+        )}
       </div>
     </div>
   );
