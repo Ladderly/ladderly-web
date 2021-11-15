64e17c1ba41cc829dccc0cccb22ca5f4cf28c26e
Lakshya Kumar <lkkumar544@gmail.com>
2021-11-15 22:54:12 +0530

    Refactored AppContainerPage

--- ladderly-react/src/pages/AppContainer/AppContainer.page.tsx
+++ ladderly-react/src/pages/AppContainer/AppContainer.page.tsx
@@ -1,9 +1,19 @@
 import React, { FC, memo } from "react";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import LaddersPage from "./Ladders.page";
 
 interface Props {}
 
 const AppContainer: FC<Props> = (props) => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/ladders" exact component={LaddersPage} />
+      </Switch>
+    </>
+  );
 };
 
 AppContainer.defaultProps = {};
