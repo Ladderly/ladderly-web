32e99b7d9fc9d77f0fd4ea04f98345619e732bfc
Lakshya Kumar <lkkumar544@gmail.com>
2021-11-17 19:22:46 +0530

    Setup routes for auth section

--- ladderly-react/src/pages/Auth/Auth.page.tsx
+++ ladderly-react/src/pages/Auth/Auth.page.tsx
@@ -1,9 +1,19 @@
 import React, { FC, memo } from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginPage from "./Login.page";
+import SignupPage from "./Signup.page";
 
 interface Props {}
 
 const Auth: FC<Props> = (props) => {
-  return <div>Auth</div>;
+  return (
+    <>
+      <Switch>
+        <Route path="/signin" component={LoginPage} />
+        <Route path="/signup" component={SignupPage} />
+      </Switch>
+    </>
+  );
 };
 
 Auth.defaultProps = {};
